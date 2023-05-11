5b78412d702968df7374790ad19d1aca2efca0d3
TheRizzy <dawidjanic1@gmail.com>
2023-05-11 17:55:05 +0200

    Create model ReserveRoom

--- ConfBooker/main/models.py
+++ ConfBooker/main/models.py
@@ -6,3 +6,12 @@ class ConferenceRoom(models.Model):
     name_room = models.CharField(max_length=255, unique=True)
     capacity_room = models.IntegerField(null=False)
     projector_available = models.BooleanField()
+
+
+class ReserveRoom(models.Model):
+    date_reservation = models.DateField()
+    id_room = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE)
+    comment_reservation = models.CharField()
+
+    class Meta:
+        unique_together = ('date_reservation', 'id_room')
